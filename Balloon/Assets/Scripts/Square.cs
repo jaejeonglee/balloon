50b2bf9fd45e8877893d414238ac66ff8c60ced8
ljj <ljj5256@gmail.com>
2022-09-20 22:12:06 +0900

    Create Square.cs

---
+++ Balloon/Assets/Scripts/Square.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Square : MonoBehaviour
+{
+    //변수 선언 
+    int Type;
+    float Size;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float x = Random.Range(-2.7f, 2.7f);
+        float y = Random.Range(3.0f, 5.0f);
+        transform.position = new Vector3(x, y, 0);
+
+        //타입을 랜덤으로 1~3 
+        Type = Random.Range(1, 4);
+
+        if (Type == 1)
+        {
+            Size = 1.3f;
+
+        }
+        else if (Type == 2)
+        {
+            Size = 1.0f;
+
+        }
+        else
+        {
+            Size = 0.7f;
+
+        }
+        //스케일을 바꾼다
+        transform.localScale = new Vector3(Size, Size, 0);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //collider 이벤트
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Circle")
+        {
+            Debug.Log("동그라미!");
+        }
+        if (coll.gameObject.tag == "Balloon")
+        {
+            Debug.Log("풍선!");
+        }
+    }
+}
