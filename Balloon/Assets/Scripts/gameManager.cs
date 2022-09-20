f81ae91215360812d7f248169bfbd7a1dbf8d20a
ljj <ljj5256@gmail.com>
2022-09-20 22:12:02 +0900

    Create gameManager.cs

---
+++ Balloon/Assets/Scripts/gameManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameManager : MonoBehaviour
+{
+
+    public GameObject Square;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("FallingSquare", 0, 0.5f);//0초 뒤에 0.5초마다 함수실행  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FallingSquare()
+    {
+        Instantiate(Square);//복제  
+    }
+}
