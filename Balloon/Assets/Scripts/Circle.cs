9fddad8997dc8da19b4d535707402ab37d2b8f40
ljj <ljj5256@gmail.com>
2022-09-23 21:12:15 +0900

    Add MousePosition for Circle object

--- Balloon/Assets/Scripts/Circle.cs
+++ Balloon/Assets/Scripts/Circle.cs
@@ -13,6 +13,7 @@ public class Circle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(mousePos.x, mousePos.y, 0);
     }
 }
